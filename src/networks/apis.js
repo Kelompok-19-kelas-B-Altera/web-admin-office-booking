256cc380cbac13208ceb0112e217c162be5203e8
syukronarie <syukronarie@gmail.com>
2022-06-17 19:58:58 +0700

    fix: baseurl dev tool

---
+++ src/networks/apis.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import CONST from '../utils/constants';
+
+const isDev = process.env.NODE_ENV === 'development';
+
+const isLocalDev = (isDev) => {
+	let axiosConfig;
+	axiosConfig = axios;
+	if (isDev) {
+		const config = {
+			baseURL: CONST.BASE_URL_API
+		};
+		axiosConfig = axios.create(config);
+	}
+	return axiosConfig;
+};
+
+const axiosInstance = isLocalDev(isDev);
+console.log(axiosInstance);
+export default axiosInstance;
