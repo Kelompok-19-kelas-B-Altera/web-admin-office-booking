b2f51bd5b6d0cd9a064ed6ebf4e1c6afc96c508f
nibnualdi <nibnualdi@gmail.com>
2022-06-30 20:00:31 +0700

    add: component content header

---
+++ src/components/ContentHeader/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const ContentHeader = ({ title }) => {
+  const pathUrlArr = window.location.pathname.split("/");
+
+  return (
+    <header className="w-[1092px] h-[52px] flex justify-between items-center px-3 bg-white mb-8">
+      <h1 className="text-2xl font-semibold text-[#070723] capitalize">{title}</h1>
+      <div className="flex">
+        <p className={pathUrlArr[1] === "" ? "text-[#070723]" : "text-[#838391]"}>Dashboard</p>
+        {pathUrlArr.slice(1).map((e) => (
+          <>
+            {pathUrlArr[1] === "" ? (
+              <></>
+            ) : (
+              <p className="capitalize">&nbsp;{`/ ${e.match("-") ? e.split("-").join(" ") : e}`}</p>
+            )}
+          </>
+        ))}
+      </div>
+    </header>
+  );
+};
+
+export default ContentHeader;
