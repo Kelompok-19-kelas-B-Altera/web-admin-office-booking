f09892801d8229a14f55c4c442ed27997195351a
nibnualdi <nibnualdi@gmail.com>
2022-07-01 13:31:20 +0700

    add: container main content

---
+++ src/components/ContentContainer/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ContentContainer = ({ children }) => {
+  return <main className="w-[1092px] h-[509px] bg-white p-3">{children}</main>;
+};
+
+export default ContentContainer;
