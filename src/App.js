15f79beba5053b09c7cfde487c915fa5770a7175
nibnualdi <nibnualdi@gmail.com>
2022-07-01 16:19:28 +0700

    fix: conflict

--- src/App.js
+++ src/App.js
@@ -2,22 +2,30 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 //Screen or Layout Web
-import { Login, Dashboard } from "./screens";
+import { Login, Dashboard, User } from "./screens";
 
 //Component
-import { TestComp, DashboardLayout, TestPage } from "./components"
+import { TestComp, DashboardLayout, TestPage } from "./components";
 
 function App() {
   return (
     <div className="bg-[#F1F1F1] min-h-screen">
-    <Router>
+      <Router>
         <Routes>
-          <Route element={<DashboardLayout/>}>
-            <Route index path='/' element={<Dashboard/>}/>
-            <Route index path='/:title' element={<TestPage/>}/>
+          <Route element={<DashboardLayout />}>
+            <Route index path="/" element={<Dashboard />} />
+            <Route index path="/:title" element={<TestPage />} />
+            <Route index path="/User" element={<User />} />
           </Route>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="*" element={<div className="flex h-screen"><p className="m-auto">There's nothing here: 404!</p></div>}/>
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="*"
+            element={
+              <div className="flex h-screen">
+                <p className="m-auto">There's nothing here: 404!</p>
+              </div>
+            }
+          />
         </Routes>
       </Router>
     </div>
