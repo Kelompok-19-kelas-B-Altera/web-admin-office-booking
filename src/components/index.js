46fc62a60a2b71eb5e282484496ac8a430beddcd
nibnualdi <nibnualdi@gmail.com>
2022-07-01 14:25:11 +0700

    add: setup some import export component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,8 @@
 // nanti daftarin componentnya disini, biar pemanggilannya gampang, test pull
 
-export {default as TestComp } from "./testComponent"
+export { default as TestComp } from "./testComponent";
+export { default as User } from "./UserComponent";
+export { default as ContentHeader } from "./ContentHeader";
+export { default as EntriesAndSearchComponent } from "./EntriesAndSearchComponent";
+export { default as ContentContainer } from "./ContentContainer";
+export { default as PaginationAndDescStatisticData } from "./PaginationAndDescStatisticData";
