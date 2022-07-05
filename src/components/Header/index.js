8f84311a5997fd76629c25249dbe0f957286e45e
nibnualdi <nibnualdi@gmail.com>
2022-07-05 18:06:58 +0700

    fix: link button to add building page & fix position button toggle sidebar (the position changed back to the wrong like before)

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,7 @@
 const Header = ({active, setActive}) => {
   return(
     <div className="flex justify-between bg-white pl-8 pr-9 py-3">
-      <div className="flex gap-[120px]">
+      <div className="w-[11.5%] flex justify-between">
         <img src="/header/logo.svg" className="self-center h-[34px]"/>
         <img src="/header/Group 29.svg" 
             className={`h-4 w-4 self-center cursor-pointer ${active ? "" : "scale-[-1]"}`}
