20c3097460d2ca0d9b27001c1aa2a152af7b734e
nibnualdi <nibnualdi@gmail.com>
2022-07-22 13:01:45 +0700

    integrasi api, belum semua. ngeutamain bisa nge list dulu hehe

--- src/screens/Office/OfficeBooked.js
+++ src/screens/Office/OfficeBooked.js
@@ -1,6 +1,107 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axiosInstance from "../../networks/apis";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  handleTotalData,
+  handleTotalPage,
+} from "../../utils/redux/features/entriesAndPaginateSlice";
 
 const OfficeBooked = () => {
+  let [listBuildings, setListBuildings] = useState([]);
+  let [listBuildingsSearched, setListBuildingsSearched] = useState([]);
+  const [triggerRequestOnDelete, setTriggerRequestOnDelete] = useState(false);
+  const entries = useSelector((state) => state.entriesAndPaginationSlice.entries);
+  const page = useSelector((state) => state.entriesAndPaginationSlice.page);
+  const inputSearch = useSelector((state) => state.entriesAndPaginationSlice.inputSearch);
+  const dispatch = useDispatch();
+
+  const d = new Date();
+  const date = String(d.getDate()).length < 2 ? `${"0" + d.getDate()}` : `${d.getDate()}`;
+  const month = String(d.getMonth()).length < 2 ? `${"0" + d.getMonth()}` : `${d.getMonth()}`;
+  const year =
+    String(d.getFullYear()).length < 2 ? `${"0" + d.getFullYear()}` : `${d.getFullYear()}`;
+  const hour = String(d.getHours()).length < 2 ? `${"0" + d.getHours()}` : `${d.getHours()}`;
+  const minute =
+    String(d.getMinutes()).length < 2 ? `${"0" + d.getMinutes()}` : `${d.getMinutes()}`;
+  const second =
+    String(d.getSeconds()).length < 2 ? `${"0" + d.getSeconds()}` : `${d.getSeconds()}`;
+  const tenMoreYears =
+    String(d.getFullYear()).length < 2
+      ? `${"0" + d.getFullYear() + 10}`
+      : `${d.getFullYear() + 10}`;
+
+  useEffect(() => {
+    if (!inputSearch || !listBuildingsSearched) {
+      axiosInstance
+        .post("/api/v1/building/search", {
+          filters: [
+            {
+              key: "isBooked",
+              join: "scheduleList",
+              operator: "EQUAL",
+              field_type: "BOOLEAN",
+              value: true,
+            },
+          ],
+          sorts: [],
+          page: page,
+          size: entries,
+        })
+        .then((res) => {
+          // console.log(res.data.data.content);
+          dispatch(handleTotalPage(res.data.data.totalPages));
+          dispatch(handleTotalData(res.data.data.totalElements));
+          setListBuildings(res.data.data.content);
+          // res.data.data.content.forEach((element) => {
+          //   if (!listBuildings.includes(element)) {
+          //     setListBuildings([...listBuildings, element]);
+          //   }
+          // });
+        });
+      // console.log("triggered");
+    } else {
+      setListBuildings([]);
+    }
+    console.log("not");
+  }, [entries, page, triggerRequestOnDelete, inputSearch]);
+
+  useEffect(() => {
+    console.log("yes");
+    if (!listBuildings || inputSearch) {
+      axiosInstance
+        .post("/api/v1/building/search", {
+          filters: [
+            {
+              key: "buildingName",
+              join: "",
+              operator: "LIKE",
+              field_type: "STRING",
+              value: inputSearch,
+            },
+            {
+              key: "isBooked",
+              join: "scheduleList",
+              operator: "EQUAL",
+              field_type: "BOOLEAN",
+              value: true,
+            },
+          ],
+          sorts: [],
+          page: page,
+          size: entries,
+        })
+        .then((res) => {
+          // console.log(res.data.data);
+          dispatch(handleTotalPage(res.data.data.totalPages));
+          dispatch(handleTotalData(res.data.data.totalElements));
+          setListBuildingsSearched(res.data.data.content);
+        });
+      console.log("triggered");
+    } else {
+      setListBuildingsSearched([]);
+    }
+  }, [entries, page, triggerRequestOnDelete, inputSearch]);
+
   return (
     <div className="p-[10px]">
       <div className="flex text-base font-semibold text-[#070723]">
@@ -19,33 +120,67 @@ const OfficeBooked = () => {
       </div>
 
       <div className="min-h-[308px]">
-        {/* Lunas */}
-        <div className="flex justify-self-auto text-base text-[#070723] mt-[12px]">
-          <p className="mr-[32px] w-[21px] my-auto text-center">1</p>
-          <div className="flex items-center w-[282px] mr-[32px] flex-1">
-            <img
-              src="/login/bg.svg"
-              alt="photo-profile"
-              className="min-w-[50px] min-h-[50px] w-[50px] h-[50px] rounded-full object-cover mr-[32px]"
-            />
-            <p className="w-[250px] capitalize">Contoh nama</p>
-          </div>
-          <p className="mr-[32px] w-[200px] my-auto capitalize flex-1">Cilandak, Jakarta Selatan</p>
-          <p className="mr-[32px] w-[100px] my-auto capitalize">Feb 10, 2022</p>
-          <p className="mr-[32px] w-[100px] my-auto capitalize">Dec 10, 2022</p>
-          <div className="mr-[32px] w-[100px] m-auto">
-            <p className="mr-[32px] w-[67px] h-[27px] rounded-[100px] bg-[#4caf50] flex justify-center items-center text-base text-white my-auto capitalize">
-              Lunas
-            </p>
-          </div>
-          <div className="flex justify-center gap-2 w-[106px]">
-            <button className="w-[98px] h-[43px] bg-[#F3C319] rounded text-white">Edit</button>
-          </div>
-        </div>
-        {/* Lunas */}
-
+        {!listBuildings || inputSearch ? (
+          <>
+            {listBuildingsSearched.map((e, index) => (
+              <div className="flex justify-self-auto text-base text-[#070723] mt-[12px]">
+                <p className="mr-[32px] w-[21px] my-auto text-center">{index + 1}</p>
+                <div className="flex items-center w-[282px] mr-[32px] flex-1">
+                  <img
+                    src={e.images[e.images.length - 1]?.image_url}
+                    alt="photo-profile"
+                    className="min-w-[50px] min-h-[50px] w-[50px] h-[50px] rounded-full object-cover mr-[32px]"
+                  />
+                  <p className="w-[250px] capitalize">{e.building_name}</p>
+                </div>
+                <p className="mr-[32px] w-[200px] my-auto capitalize flex-1">{e.address}</p>
+                <p className="mr-[32px] w-[100px] my-auto capitalize">Feb 10, 2022</p>
+                <p className="mr-[32px] w-[100px] my-auto capitalize">Dec 10, 2022</p>
+                <div className="mr-[32px] w-[100px] m-auto">
+                  <p className="mr-[32px] w-[67px] h-[27px] rounded-[100px] bg-[#4caf50] flex justify-center items-center text-base text-white my-auto capitalize">
+                    Lunas
+                  </p>
+                </div>
+                <div className="flex justify-center gap-2 w-[106px]">
+                  <button className="w-[98px] h-[43px] bg-[#F3C319] rounded text-white">
+                    Edit
+                  </button>
+                </div>
+              </div>
+            ))}
+          </>
+        ) : (
+          <>
+            {listBuildings.map((e, index) => (
+              <div className="flex justify-self-auto text-base text-[#070723] mt-[12px]">
+                <p className="mr-[32px] w-[21px] my-auto text-center">{index + 1}</p>
+                <div className="flex items-center w-[282px] mr-[32px] flex-1">
+                  <img
+                    src={e.images[e.images.length - 1]?.image_url}
+                    alt="photo-profile"
+                    className="min-w-[50px] min-h-[50px] w-[50px] h-[50px] rounded-full object-cover mr-[32px]"
+                  />
+                  <p className="w-[250px] capitalize">{e.building_name}</p>
+                </div>
+                <p className="mr-[32px] w-[200px] my-auto capitalize flex-1">{e.address}</p>
+                <p className="mr-[32px] w-[100px] my-auto capitalize">Feb 10, 2022</p>
+                <p className="mr-[32px] w-[100px] my-auto capitalize">Dec 10, 2022</p>
+                <div className="mr-[32px] w-[100px] m-auto">
+                  <p className="mr-[32px] w-[67px] h-[27px] rounded-[100px] bg-[#4caf50] flex justify-center items-center text-base text-white my-auto capitalize">
+                    Lunas
+                  </p>
+                </div>
+                <div className="flex justify-center gap-2 w-[106px]">
+                  <button className="w-[98px] h-[43px] bg-[#F3C319] rounded text-white">
+                    Edit
+                  </button>
+                </div>
+              </div>
+            ))}
+          </>
+        )}
         {/* DP */}
-        <div className="flex justify-self-auto text-base text-[#070723] mt-[12px]">
+        {/* <div className="flex justify-self-auto text-base text-[#070723] mt-[12px]">
           <p className="mr-[32px] w-[21px] my-auto text-center">2</p>
           <div className="flex items-center w-[282px] mr-[32px] flex-1">
             <img
@@ -66,7 +201,7 @@ const OfficeBooked = () => {
           <div className="flex justify-center gap-2 w-[106px]">
             <button className="w-[98px] h-[43px] bg-[#F3C319] rounded text-white">Edit</button>
           </div>
-        </div>
+        </div> */}
         {/* DP */}
       </div>
     </div>
